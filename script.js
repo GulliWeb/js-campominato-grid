0264d7257386cdc57b309a5e9a290f364ffd64ba
Andrea Gulli <andreagulli11@gmail.com>
2024-08-23 18:06:18 +0200

    completed MILESTONE 2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const container = document.getElementById('grid-container')
 
-for (let i = 1; i <= 81; i++) {
-
-
-    const grid = document.createElement('div')
-    grid.classList.add('grid')
-    grid.innerText = i
-    container.appendChild(grid)
+// Al click del pulsante play genero 100 caselle
+function play() {
+    for (let i = 1; i <= 100; i++) {
+        const grid = document.createElement('div')
+        grid.classList.add('grid', 'grid-100')
+        grid.innerText = i
+        container.appendChild(grid)
+    }    
 }
-
 
