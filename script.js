ba0f0ed2fc018c999be4865fde032f266758bfaa
Andrea Gulli <andreagulli11@gmail.com>
2024-08-24 10:46:57 +0200

    completed MILESTONE 4

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Raccolta dati dal DOM
 const container = document.getElementById('grid-container')
-
 // Al click del pulsante play genero 100 caselle
 function play() {
     if (container.querySelectorAll('.grid').length >= 100) {
+        // location.reload()
         return
     }
 
@@ -12,8 +12,16 @@ function play() {
         grid.classList.add('grid', 'grid-100')
         grid.innerText = i
         container.appendChild(grid)
-    }    
 
+        grid.addEventListener('click' ,function() {
+            cellClick(grid, i)
+        })
+    }    
     document.querySelector('footer').classList.add('d-visible')
 }
 
+// Al click di ogni cella stampiamo in console il numero di essa e coloriamola di azzurro
+function cellClick(grid, index) {
+    grid.classList.add('azure-click', 'click')   
+    console.log(index) 
+}
